e3ffc9573c07d6be2ad7073dbd491b70041a9f65
Rex2626 <rexattwood26@gmail.com>
2022-05-19 15:30:13 +0100

    Test init

---
+++ objects.py
@@ -0,0 +1,85 @@
+import pygame
+import random
+
+class Padel():
+    def __init__(self, x, y, width, height) -> None:
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+
+
+class Ball():
+    def __init__(self, width, height, ball_width, ball_height, speed) -> None:
+        self.x = width // 2
+        self.y = random.randint(0.01 * height, 0.99 * height)
+        self.width = ball_width
+        self.height = ball_height
+        vx = random.random() * (0.8 * speed**2) + 0.1 * speed**2
+        vy = speed**2 - vx
+        self.vx, self.vy = vx**0.5, vy**0.5
+        self.vx *= random.randint(0, 1) * 2 - 1
+
+    def rect(self):
+        return pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+
+    def collision_red(self, padel: Padel):
+        x, y = self.x, self.y
+        if padel.x + padel.width - x > padel.y + padel.height - y: # Top left ball to bottom right padel
+            self.vy = abs(self.vy)
+            x, y = self.x, self.y + self.height
+        elif padel.x + padel.width - x > y - padel.y: # Bottom left ball to top right padel
+            self.vy = -abs(self.vy)
+        else:
+            self.vx = abs(self.vx)
+
+    def collision_yellow(self, padel: Padel):
+        x, y = self.x + self.width, self.y
+        if padel.x - x > padel.y + padel.height - y: # Top right ball to bottom left padel
+            self.vy = abs(self.vy)
+            x, y = self.x + self.width, self.y + self.height
+        elif padel.x - x > y - padel.y: # Bottom right ball to top left padel
+            self.vy = -abs(self.vy)
+        else:
+            self.vx = -abs(self.vx)
+
+    def increase_speed(self):
+        self.vx *= 1.1
+        self.vy *= 1.1
+
+    def boundary_collision(self, height):
+        if self.y < 0 or self.y + self.height > height:
+            self.vy *= -1
+
+    def collide_padel(self, padel: Padel):
+        x, y = self.x, self.y
+        if x < padel.x + padel.width and x > padel.x and y < padel.y + padel.height and y > padel.y:
+            return True
+        x += self.width
+        if x < padel.x + padel.width and x > padel.x and y < padel.y + padel.height and y > padel.y:
+            return True
+        y += self.height
+        if x < padel.x + padel.width and x > padel.x and y < padel.y + padel.height and y > padel.y:
+            return True
+        x = self.x
+        if x < padel.x + padel.width and x > padel.x and y < padel.y + padel.height and y > padel.y:
+            return True
+        return False
+
+    def scored(self, width):
+        if self.x < 1 or self.x + self.width > width:
+            return True
+        return False
+
+    def restart(self, width, height, speed):
+        self.x = width // 2
+        self.y = random.randint(0.01 * height, 0.99 * height)
+        vx = random.random() * (0.8 * speed**2) + 0.1 * speed**2
+        vy = speed**2 - vx
+        self.vx, self.vy = vx**0.5, vy**0.5
+        self.vx *= random.randint(0, 1) * 2 - 1
---
+++ pong.py
@@ -0,0 +1,112 @@
+import pygame
+import os
+from objects import Ball, Padel
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("GamingX Pong")
+
+FPS = 500
+SPEED = 250 / FPS
+speed = SPEED
+last_collided = None
+"test"
+
+BLACK = (0, 0, 0)
+PADEL_WIDTH, PADEL_HEIGHT = 13, 55
+RED_PADEL_X = 100 - PADEL_WIDTH // 2
+YELLOW_PADEL_X = 800 - PADEL_WIDTH // 2
+PADEL_Y = 250 - PADEL_HEIGHT // 2
+
+YELLOW_PADEL_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'padel_yellow.png'))
+YELLOW_PADEL = pygame.transform.scale(
+    YELLOW_PADEL_IMAGE, (PADEL_WIDTH, PADEL_HEIGHT))
+
+RED_PADEL_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'padel_red.png'))
+RED_PADEL = pygame.transform.scale(
+    RED_PADEL_IMAGE, (PADEL_WIDTH, PADEL_HEIGHT))
+
+BALL_WIDTH, BALL_HEIGHT = 8, 8
+BALL_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'ball.png'))
+BALL = pygame.transform.scale(
+    BALL_IMAGE, (BALL_WIDTH, BALL_HEIGHT))
+
+
+def draw_window(yellow: pygame.Rect, red: pygame.Rect, ball: Ball):
+    WIN.fill(BLACK)
+    WIN.blit(YELLOW_PADEL, (yellow.x, yellow.y))
+    WIN.blit(RED_PADEL, (red.x, red.y))
+    WIN.blit(BALL, (ball.x, ball.y))
+    pygame.display.update()
+
+
+def red_handle_movement(keys_pressed, red: pygame.Rect):
+    if keys_pressed[pygame.K_w] and red.y - speed > 10:  # UP
+        red.y -= speed
+    if keys_pressed[pygame.K_s] and red.y + speed + red.height < HEIGHT - 10:  # DOWN
+        red.y += speed
+
+
+def yellow_handle_movement(keys_pressed, yellow: pygame.Rect):
+    if keys_pressed[pygame.K_UP] and yellow.y - speed > 10:  # UP
+        yellow.y -= speed
+    if keys_pressed[pygame.K_DOWN] and yellow.y + speed + yellow.height < HEIGHT - 10:  # DOWN
+        yellow.y += speed
+
+
+def handle_ball_movement(ball: Ball, yellow: pygame.Rect, red: pygame.Rect):
+    global speed
+    global last_collided
+    ball.move()
+
+    if ball.collide_padel(red):
+        ball.collision_red(red)
+        if last_collided == yellow:
+            speed *= 1.1
+            ball.increase_speed()
+            last_collided = red
+
+    elif ball.collide_padel(yellow):
+        ball.collision_yellow(yellow)
+        if last_collided == red:
+            speed *= 1.1
+            ball.increase_speed()
+            last_collided = yellow
+
+    ball.boundary_collision(HEIGHT)
+
+    if ball.scored(WIDTH):
+        ball.restart(WIDTH, HEIGHT, SPEED)
+        speed = SPEED
+
+
+def main():
+    red = Padel(RED_PADEL_X, PADEL_Y, PADEL_WIDTH, PADEL_HEIGHT)
+    yellow = Padel(YELLOW_PADEL_X, PADEL_Y, PADEL_WIDTH, PADEL_HEIGHT)
+
+    ball = Ball(WIDTH, HEIGHT, BALL_WIDTH, BALL_HEIGHT, SPEED)
+
+    run = True
+    clock = pygame.time.Clock()
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                
+        keys_pressed = pygame.key.get_pressed()
+        red_handle_movement(keys_pressed, red)
+        yellow_handle_movement(keys_pressed, yellow)
+
+        handle_ball_movement(ball, yellow, red)
+
+        draw_window(yellow, red, ball)
+
+
+if __name__ == "__main__":
+    main()
