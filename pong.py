ffa808ae3094678079123986cfbefc85333b18d4
Rex2626 <rexattwood26@gmail.com>
2022-05-26 15:09:13 +0100

    Something may have been deleted

--- pong.py
+++ pong.py
@@ -211,6 +211,7 @@ def main(red_handle_movement, menu: "_menu.Menu"):
             max_x=WIDTH * POWERUP_MAX_X_RATIO,
             min_y=(HEIGHT - TEXT_BAR_HEIGHT) * POWERUP_MIN_Y_RATIO + TEXT_BAR_HEIGHT,
             max_y=(HEIGHT - TEXT_BAR_HEIGHT) * POWERUP_MAX_Y_RATIO + TEXT_BAR_HEIGHT,
+            other_powerups_present=powerups
         ))
 
     running = True
